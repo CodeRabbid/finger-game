df2c85d2a1538c404fd520ae769e873a54c6fbd8
Artiyom <artiyom.moskalew.2@gmail.com>
2024-05-15 21:33:28 +0200

    add third button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   const [showBlue, setShowBlue] = useState(false);
   const [st1, setSt1] = useState(false);
   const [st2, setSt2] = useState(false);
+  const [st3, setSt3] = useState(false);
 
   return (
     <div
@@ -37,6 +38,18 @@ function App() {
         }}
         onPointerDown={() => setSt2(true)}
         onPointerUp={() => setSt2(false)}
+      ></div>
+      <div
+        style={{
+          left: 30,
+          top: 130,
+          height: 40,
+          width: 40,
+          backgroundColor: "red",
+          position: "absolute",
+        }}
+        onPointerDown={() => setSt3(true)}
+        onPointerUp={() => setSt3(false)}
       ></div>
       {st1 && st2 && (
         <div
