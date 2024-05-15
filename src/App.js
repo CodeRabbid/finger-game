ee7f85fefbe3f086b378c66b8ccb644d677a6df6
Artiyom <artiyom.moskalew.2@gmail.com>
2024-05-15 21:39:37 +0200

    fix bug

--- src/App.js
+++ src/App.js
@@ -51,20 +51,18 @@ function App() {
         onPointerDown={() => setSt3(true)}
         onPointerUp={() => setSt3(false)}
       ></div>
-      {st1 &&
-        st2 &&
-        st3(
-          <div
-            style={{
-              left: 130,
-              top: 170,
-              height: 40,
-              width: 40,
-              backgroundColor: "blue",
-              position: "absolute",
-            }}
-          ></div>
-        )}
+      {st1 && st2 && st3 && (
+        <div
+          style={{
+            left: 130,
+            top: 170,
+            height: 40,
+            width: 40,
+            backgroundColor: "blue",
+            position: "absolute",
+          }}
+        ></div>
+      )}
     </div>
   );
 }
